1d871f816a1790162182ac2c0bece67483c0f63c
jumpydave <wjdavenport2004@gmail.com>
2024-06-02 16:48:08 +1000

    implemented popup menu

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 pogchampion
+
+function openForm() {
+    document.getElementById("signUpForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("signUpForm").style.display = "none";
+}
