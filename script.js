87e1271fdcffb71b28f77fce57b40c82c09badc2
jumpydave <wjdavenport2004@gmail.com>
2024-06-03 16:27:53 +1000

    got toggles half working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let topLeftToggle = 0;
+let topRightToggle = 0;
 
 function openForm() {
     document.getElementById("signUpForm").style.display = "flex";
@@ -9,14 +10,36 @@ function closeForm() {
 }
 
 function topLeftSelect() {
-    alert(topLeftToggle)
     elem = document.getElementById("top-left");
 
     if (topLeftToggle == 0) {
-    elem.style.backgroundColor = "red";
+    elem.style.backgroundColor = "#FCB017";
+    topLeftToggle = 1;    
     }else{
         elem.style.backgroundColor = "white";
-        topLeftToggle = 1;
+        topLeftToggle = 0;
+    }
+
+    if (topRightToggle == 1) {
+        document.getElementById("top-right").style.backgroundColor = "white";
+        topRightToggle = 0;    
+    }
+}
+
+function topRightSelect() {
+    elem = document.getElementById("top-right");
+
+    if (topRightToggle == 0) {
+    elem.style.backgroundColor = "#FCB017";
+    topRightToggle = 1;    
+    }else{
+        elem.style.backgroundColor = "white";
+        topRightToggle = 0;
+    }
+
+    if (topLeftToggle == 1) {
+        document.getElementById("top-left").style.backgroundColor = "white";
+        topLeftToggle = 0;    
     }
 
 }
