94827a209e1ec8aafde1d2a626ef88589ed16a81
jumpydave <wjdavenport2004@gmail.com>
2024-06-03 16:33:11 +1000

    finished coding selection for kit style

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let topLeftToggle = 0;
 let topRightToggle = 0;
+let bottomLeftToggle = 0;
+let bottomRightToggle = 0;
 
 function openForm() {
     document.getElementById("signUpForm").style.display = "flex";
@@ -40,6 +42,41 @@ function topRightSelect() {
     if (topLeftToggle == 1) {
         document.getElementById("top-left").style.backgroundColor = "white";
         topLeftToggle = 0;    
+    }
+
+}
+
+function bottomLeftSelect() {
+    elem = document.getElementById("bottom-left");
+
+    if (bottomLeftToggle == 0) {
+    elem.style.backgroundColor = "#FCB017";
+    bottomLeftToggle = 1;    
+    }else{
+        elem.style.backgroundColor = "white";
+        bottomLeftToggle = 0;
+    }
+
+    if (bottomRightToggle == 1) {
+        document.getElementById("bottom-right").style.backgroundColor = "white";
+        bottomRightToggle = 0;    
+    }
+}
+
+function bottomRightSelect() {
+    elem = document.getElementById("bottom-right");
+
+    if (bottomRightToggle == 0) {
+    elem.style.backgroundColor = "#FCB017";
+    bottomRightToggle = 1;    
+    }else{
+        elem.style.backgroundColor = "white";
+        bottomRightToggle = 0;
+    }
+
+    if (bottomLeftToggle == 1) {
+        document.getElementById("bottom-left").style.backgroundColor = "white";
+        bottomLeftToggle = 0;    
     }
 
 }
